4e79997c20e79474371aa5d248c3bb83886f3e43
Limaisaias-ux <54186829+Limaisaias-ux@users.noreply.github.com>
2019-08-16 17:08:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -9,9 +9,21 @@ try
 {
   $pdo = new PDO($dsn, $dbuser, $dbpass);
     echo "Conexão estabelecida com sucesso!";
+    
+    $sql = "SELECT * FROM posts";
+    $dado = $pdo->query($sql);
+    
+    if($dado->rowCount() > 0){
+        echo "<h3>Há posts cadastrados</h3>";
+    }
+    else{
+        echo "<h3>Não há posts cadastrados</h3>";
+    }
+    
+
 }
 catch(PDOExcption $e)
 {
-    echo "Falhou: ".$e->getMessage();
+    echo "<h2>Falhou: ".$e->getMessage()."</h2>";
 }
 
